b008474dd5001707252fbb39260139b17a10d8de
cheol <qjel60@naver.com>
2023-01-31 15:31:26 +0900

    feat:boardwrite

--- front/routes/board.route.js
+++ front/routes/board.route.js
@@ -11,46 +11,23 @@ route.get("/", (req, res) => {
     res.render("board/main.html");
 });
 
-// route.post("/array", async (req, res) => {
-//     const body = req.body;
-//     console.log(body);
-//     // const respons = await request.post("/board/array", body, {
-//     //     headers: {
-//     //         ["Content-Type"]: "multipart/form-data",
-//     //     },
-//     // });
-//     // console.log(respons);
-// });
-
 route.post("/", async (req, res) => {
     const { userid } = req.user;
     const { hashtag: text } = req.body;
+    console.log(req.body);
     const respone = text.split(",");
     const body = {
         ...req.body,
         hashtag: respone,
         userid,
     };
-    console.log(body);
-
     const respones = await request.post("/boards", body);
-    // console.log(respones);
     res.redirect("board");
 });
-
-// route.post("/array", (req, res) => {
-//     console.log(req.body);
-//     console.log(req.file);
-// });
 
 route.get("/write", (req, res) => {
     res.render("board/write.html");
 });
-
-// route.get("/:id", (req, res) => {
-//     res.render("board/idview.html");
-// });
-
 route.get("/:id/:idx", (req, res) => {
     res.render("board/view.html");
 });
