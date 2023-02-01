1dd3b63f99a9844392bc9306d644fa9efdefafd0
o-ogie <wrld.worthy@gmail.com>
2023-02-01 11:32:16 +0900

    fix: pull 전 commit

--- front/config.js
+++ front/config.js
@@ -2,13 +2,6 @@ const config = {
     BACK_HOST: "127.0.0.1",
     // BACK_HOST: "54.180.163.189",
     PORT: process.env.PORT || 3000,
-<<<<<<< HEAD
-    // PORT: process.env.PORT || 80,
-  };
-  
-module.exports = config
-=======
-};
+}
 
 module.exports = config;
->>>>>>> pkc
