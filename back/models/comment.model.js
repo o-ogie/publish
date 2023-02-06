d664da347aa712a349d57f00f5e850fdf1ec209e
cheol <qjel60@naver.com>
2023-02-06 10:15:17 +0900

    fix:models

--- back/models/comment.model.js
+++ back/models/comment.model.js
@@ -1,31 +1,40 @@
 module.exports = (sequelize, Sequelize) => {
     class Comment extends Sequelize.Model {
-      static createTable() {
-        return this.init(
-          {
-            content: {
-              type: Sequelize.TEXT(),
-              allowNull: false,
-            },
-          },
-          {
-            sequelize,
-            timestamp: true
-          }
-        );
-      }
-      static associate(models) {
-          this.belongsTo(models.Board, {
-              foreignKey: "boardid",
-          })
-          this.belongsTo(models.User, {
-              foreignKey: "userid"
-          })
-          this.hasMany(models.PointUp, {
-            foreignKey: "commentid",
-          });
-      }
+        static createTable() {
+            return this.init(
+                {
+                    content: {
+                        type: Sequelize.TEXT(),
+                        allowNull: false,
+                    },
+                    depth: {
+                        type: Sequelize.INTEGER,
+                        allowNull: false,
+                        defaultValue: 0,
+                    },
+                    group: {
+                        type: Sequelize.INTEGER,
+                        allowNull: false,
+                    },
+                },
+                {
+                    sequelize,
+                    timestamp: true,
+                }
+            );
+        }
+        static associate(models) {
+            this.belongsTo(models.Board, {
+                foreignKey: "boardid",
+            });
+            this.belongsTo(models.User, {
+                foreignKey: "userid",
+            });
+            this.hasMany(models.PointUp, {
+                foreignKey: "commentid",
+            });
+        }
     }
     Comment.createTable();
-  };
-  
+};
+
