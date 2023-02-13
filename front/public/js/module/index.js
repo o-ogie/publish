ef576294497eff4bf3b8a8ff89e419cb6f40a5b0
cheol <qjel60@naver.com>
2023-02-13 19:41:30 +0900

    fix:index.js

--- front/public/js/module/index.js
+++ front/public/js/module/index.js
@@ -6,20 +6,20 @@ sortSwitch.value = location.search.indexOf("sort") === -1 ? "A.id" : location.se
 categorySwitch.value = location.search.indexOf("category") === -1 ? "default" : location.search.split("=")[1];
 
 // intersection observer
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((v) => {
-        if (v.isIntersecting) {
-            const target = v.target;
-            observer.unobserve(target);
-            // 다음 9개의 li 요소를 화면에 그리는 코드
-        }
-    });
-});
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((v) => {
+//         if (v.isIntersecting) {
+//             const target = v.target;
+//             observer.unobserve(target);
+//             // 다음 9개의 li 요소를 화면에 그리는 코드
+//         }
+//     });
+// });
 
-const items = document.querySelectorAll(".boardItem");
-items.forEach((target) => {
-    observer.observe(target);
-});
+// const items = document.querySelectorAll(".boardItem");
+// items.forEach((target) => {
+//     observer.observe(target);
+// });
 // 글 감추기
 const blindBtn = document.querySelector(".hideItem");
 const itemId = document.querySelectorAll("#itemId");
