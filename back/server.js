0f71787d4f27719975c36a2f875c857b8881b91d
o-ogie <wrld.worthy@gmail.com>
2023-02-14 11:05:17 +0900

    fix: NodeJs 서버시간 차이, DB createdAt 시간  수정

--- back/server.js
+++ back/server.js
@@ -6,13 +6,13 @@ const SocketIO = require("./socket.io");
 console.log(config);
 
 const server = app.listen(PORT, async () => {
-    await sequelize.sync({ force: true });
+    await sequelize.sync({ force: false });
 
-    await sequelize.models.Category.create({ category: "food" });
-    await sequelize.models.Category.create({ category: "tech" });
-    await sequelize.models.Category.create({ category: "default" });
-    await sequelize.models.Category.create({ category: "travel" });
-    await sequelize.models.Category.create({ category: "life" });
+    // await sequelize.models.Category.create({ category: "food" });
+    // await sequelize.models.Category.create({ category: "tech" });
+    // await sequelize.models.Category.create({ category: "default" });
+    // await sequelize.models.Category.create({ category: "travel" });
+    // await sequelize.models.Category.create({ category: "life" });
 
     console.log(`backend server listening on port ${PORT}`);
 });
